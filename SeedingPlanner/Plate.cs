d7e4de078e5bd3fad2637eae97bd5c65370b3d57
minagaba <minagaba@gmail.com>
2015-02-24 22:58:12 +0200

    qq

---
+++ SeedingPlanner/Plate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SeedingPlanner
+{
+    class Plate
+    {
+        // configuration settings
+        public static int SAMPLES_COUNT = Convert.ToInt32(ConfigurationManager.AppSettings["Plate_SamplesCount"]);
+
+        public string Name { get; set; }
+
+        private int _seedsCount;
+        public int SeedsCount { get { return _seedsCount; } }
+        
+        // samples to take from this tray
+        private SortedSet<string> _samples;
+        public int NumberOfSamples { get { return _samples.Count; } }
+
+        private HashSet<Tray> _trays;
+
+
+        public Plate(string name)
+        {
+            Name = name;
+            _seedsCount = 0;
+            _samples = new SortedSet<string>();
+            _trays = new HashSet<Tray>();
+        }
+
+        public bool isFull()
+        {
+            return _seedsCount >= SAMPLES_COUNT;
+        }
+        
+        public int AddSamples(Tray tray, Bag bag)
+        {
+            return AddSamples(tray, bag, bag.SeedsToSample);
+        }
+
+        public int AddSamples(Tray tray, Bag bag, int seedsToAdd)
+        {
+            int seedsAdded = 0;
+
+            if (seedsToAdd > 0)
+            {
+                if (seedsToAdd <= (SAMPLES_COUNT - _seedsCount))
+                {
+                    seedsAdded = seedsToAdd;
+                }
+                else
+                {
+                    seedsAdded = SAMPLES_COUNT - _seedsCount;
+                }
+                _seedsCount += seedsAdded;
+                _samples.UnionWith(bag.Samples);
+                _trays.Add(tray);
+            }
+
+            return seedsAdded;
+        }
+
+    }
+}
---
+++ SeedingPlanner/SeedingPlan.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedingPlanner
+{
+    class SeedingPlan
+    {
+        //BagsInventory _bags;
+        private List<Tray> _trays;
+        private List<Plate> _plates;
+        private int[] _bagsOrder;
+
+        public SeedingPlan()
+        {
+            //_bags = bags;
+            _trays = new List<Tray>();
+            _plates = new List<Plate>();
+            _bagsOrder = null;
+        }
+
+        public void Setup(int[] bagsOrder)
+        {
+            // TODO: validate params
+
+            _bagsOrder = bagsOrder;
+
+            // populate trays and plates according to ordering
+            Tray currTray = new Tray("tray " + _trays.Count);
+            Plate currPlate = new Plate("plate " + _plates.Count);
+            for (int i = 0; i < BagsInventory.Count; ++i)
+            {
+                Bag bag = BagsInventory.GetAt(_bagsOrder[i]);
+                int addedToTray = 0;
+                int addedToPlate = 0;
+                
+                addedToTray += currTray.AddSeedsFromBag(bag);
+                if (addedToTray < bag.SeedsToPlant)
+                {
+                    // tray is full, take another tray
+                    _trays.Add(currTray);
+                    addedToPlate = currPlate.AddSamples(currTray, bag, addedToTray);
+                    if (addedToPlate < addedToTray)
+                    {
+                        _plates.Add(currPlate);
+                        currPlate = new Plate("plate " + _plates.Count);
+                        currPlate.AddSamples(currTray, bag, (addedToTray - addedToPlate));
+                    }
+
+                    currTray = new Tray("tray " + _trays.Count);
+                    currTray.AddSeedsFromBag(bag, bag.SeedsToPlant - addedToTray);
+                }
+
+                addedToPlate = currPlate.AddSamples(currTray, bag, addedToTray);
+                if (addedToPlate < addedToTray)
+                {
+                    _plates.Add(currPlate);
+                    currPlate = new Plate("plate " + _plates.Count);
+                    currPlate.AddSamples(currTray, bag, (addedToTray - addedToPlate));
+                }
+            }
+        }
+
+        public int Cost(int trayCost, int sampleCost)
+        {
+            int cost = 0;
+
+            cost += (_trays.Count * trayCost);
+            foreach (Plate p in _plates)
+            {
+                cost += (p.NumberOfSamples * p.SeedsCount);
+            }
+
+            return cost;
+        }
+    }
+}
